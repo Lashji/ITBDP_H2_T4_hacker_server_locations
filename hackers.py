6f8fb5d270cbe4ac16577f9b75c8f40ae8c517ab
lashj <lassi.palojarvi@outlook.com>
2018-11-06 17:59:07 +0200

    pie chart

--- hackers.py
+++ hackers.py
@@ -1,5 +1,7 @@
-import urllib
+import urllib.request
 import json
+import time
+import matplotlib.pyplot as plt
 
 
 def clean_data(f):
@@ -28,7 +30,42 @@ def get_ips_as_list(data):
 
 
 def do_req(ip):
-    ""
+    req = urllib.request.urlopen("https://www.iplocate.io/api/lookup/"+ip)
+    data = json.loads(req.read())
+
+    return data
+
+
+def get_country_index(data, country):
+    for i in range(len(data)):
+        if data[i]["country"] == country:
+            return i
+    return -1
+
+
+def obj_in_countries(countries, country_name):
+    for c in countries:
+        if c["country"] == country_name:
+            return True
+    return False
+
+
+def get_top_ten_countries(countries):
+    tmp = []
+
+    maxc = countries[0]
+    clist = countries
+
+    for i in range(10):
+        maxc = countries[0]
+
+        for c in clist:
+            num = c["count"]
+            if num > maxc["count"]:
+                maxc = c
+    tmp.append(maxc)
+    clist.pop(get_country_index(clist, c["country"]))
+    return tmp
 
 
 def get_ip_data(data):
@@ -36,13 +73,39 @@ def get_ip_data(data):
     countries = []
 
     for i in ip:
-        countries.append(do_req(i))
+        data = do_req(i)
 
+        if data["country"]:
+
+            obj = {
+                "country": data["country"],
+                "count": 1
+            }
+
+            if not obj_in_countries(countries, obj["country"]):
+                countries.append(obj)
+            else:
+                countries[get_country_index(
+                    countries, data["country"])]["count"] += 1
+
+        time.sleep(1)
     return countries
 
 
 def create_pie(data):
-    print("")
+
+    if len(data) > 10:
+        data = get_top_ten_countries(data)
+
+    labels = []
+    counts = []
+    for i in data:
+        labels.append(i["country"])
+        counts.append(i["count"])
+    fig1, ax1 = plt.subplots()
+    ax1.pie(counts, labels=labels, autopct='%1.1f%%')
+    ax1.axis('equal')
+    plt.show()
 
 
 def main():
